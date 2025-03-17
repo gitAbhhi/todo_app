3c80ab23e3d6deed313380b4dae18e78de0416b5
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-03-17 09:48:37 +0530

    add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const saveTols=()=>{
 const togglefinished=()=>{
   setshowfinished(!showfinished)
 }
-  const handleedit=()=>{
+  const handleedit=(e,id)=>{
     let t=todos.filter(i=>i.id===id)
     settask(t[0].task)
     let newTodos=todos.filter(item=>{
@@ -67,12 +67,12 @@ const togglefinished=()=>{
   return (
     <>
       <Navbar />
-      <div className="container m-auto md:w-[70%]  my-5 rounded-3xl p-5 bg-violet-100">
+      <div className="container m-auto md:w-[50%]  my-5 rounded-3xl p-5 bg-violet-100">
         <h1 className='text-3xl font-bold text-center mb-4 bg-violet-500 rounded-full text-white p-4'>iTask- Manage your todos at one place</h1>
         <div>
           <div className='text-2xl font-bold'>Add a Todo</div>
-          <input onChange={handlechange} value={task} className='w-[85%] h-11 my-3 rounded-full p-3' type="text" placeholder='Write your task' />
-          <button onClick={handleadd} disabled={task.length<=3} className='bg-violet-700 text-white text-xl font-bold rounded-full w-[10%] h-11 m-3 items-center'>Save</button>
+          <input onChange={handlechange} value={task} className='w-[85%] h-11 my-3 rounded-full p-3' type="text" placeholder='Write your task'  onKeyDown={(e) => e.key === "Enter" && handleadd()} />
+          <button onClick={handleadd} disabled={task.length<=3} className='bg-violet-700 text-white text-xl font-bold rounded-full w-[20%] h-11 m-3 items-center'>Save</button>
           <div>
             <input type="checkbox"onChange={togglefinished} name="show finished" id="show" /><span className='mx-2 text-base text-gray-500'>Show finished</span>
           </div> 
@@ -83,12 +83,13 @@ const togglefinished=()=>{
           
           {todos.length ===0 && <div>NO Todos to display</div>  }
           {todos.map(item=>{
-          return (showfinished|| !item.isCompleted)&& <div key={item.id} className="{task flex items-center w-1/4 justify-between my-3}">
-            <div className="flex gap-5">
-            <input name={item.id} onChange={handlecheckbox} type="checkbox" checked={item.isCompleted} id="" />
+          return (showfinished|| !item.isCompleted)&& <div key={item.id} className="{task flex items-center w-full
+           justify-between my-3}">
+            <div className="flex gap-3">
+            <input name={item.id} onChange={handlecheckbox} className='mx-2' type="checkbox" checked={item.isCompleted} id="" />
             <div className={item.isCompleted?"line-through":""}>{item.task}</div>
             </div>
-            <div className="buttons flex h-full justify-end w-full md:">
+            <div className="buttons flex h-full justify-end w-1/2 md:">
             <button onClick={(e)=>handleedit(e,item.id)} class='bg-violet-700 text-white text-sm font-bold rounded-xl p-2 h-11 ml-12 my-2 items-center'>Edit</button>
             <button onClick={(e)=>handledelete(e,item.id)} class='bg-violet-700 text-white text-sm font-bold rounded-xl  p-2 h-11 items-center my-2 mx-1'>Delete</button>
             </div>
